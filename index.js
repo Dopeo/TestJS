bc302eab5430020879156017362757f2d8548b73
Danny <128592738+Dopeo@users.noreply.github.com>
2023-05-12 02:04:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-const url = "https://script.google.com/macros/s/AKfycbxrgBV4ynhZpO2lfn5qlPdpnkAPPYaFvpS4lO0nRjVFRqDgfR3csqLcIuHRD8rOGVUIrA/exec"
+let customerForm = document.getElementById("customerForm");
+let firstName = document.getElementById("firstname");
+let lastName = document.getElementById("lastname");
+let cityName = document.getElementById("city");
+let emailAdd = document.getElementById("email");
+let submitBtn = document.getElementById("submitBtn");
+const spinner = document.getElementById("buttonSpinner");
+const btnText = document.getElementById("buttonText");
+const errorAlert = document.getElementById("errorAlert")
 
 
-function testGS(){
+function afterSubmit(e){
+  e.preventDefault();
+  if (customerForm.checkValidity() === false) {
+    event.stopPropagation();
+    for(field of customerForm.elements) {
+      if(!field.checkValidity()){
+        field.classList.add("is-invalid");
+      }
+    }
+    return;
+  }
 
-      fetch(url)
-      .then(d => d.json())
-      .then(d => {
-      document.getElementById("app").textContent = d[0].status;
-      });
+  for(field of customerForm.elements) {
+      field.classList.remove("is-invalid");
+    }
+  
+  let info = {
+    first: firstName.value,
+    last: lastName.value,
+    email: emailAdd.value,
+    city: cityName.value,
+  };
 
-}
+  const url = "https://script.google.com/macros/s/AKfycbzhBUXF7S1Oh2uGdKhpn5yCF15_pwgVgWsh26ovYUN0C4SUVKyjJBC72XowLnokmARsMQ/exec"
+  
+  spinner.classList.remove("d-none");
+  btnText.textContent = "Sending...";
+  submitBtn.disabled = true;
 
-function addGS(){
-
-
-  fetch(url, {
-      method: 'POST',
-      mode: 'no-cors',
-      cache: 'no-cache',
-      headers: {'content-Type' : 'application/json'},
-      redirect: 'follow',
-      body: JSON.stringify({first:"jake",email:"jCookie@gmail.com",last:"Cookie"})
+  fetch(url,{
+    method: 'POST',
+    cache: 'no-cache',
+    redirect: 'follow',
+    body: JSON.stringify(info)
+  })
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+    customerForm.reset();
+    spinner.classList.add("d-none");
+    btnText.textContent = "Send";
+    submitBtn.disabled = false;
+  })
+  .catch(err => {
+    console.log(err);
+    console.log("Something Went Wrong!");
+    errorAlert.classList.remove("d-none");
+    setTimeout(function(){
+      errorAlert.classList.add("d-none");
+      spinner.classList.add("d-none");
+      btnText.textContent = "Send";
+      submitBtn.disabled = false;
+    },3000);
   });
+};
 
-}
-
-document.getElementById("btn2").addEventListener("click", addGS);
-document.getElementById("btn").addEventListener("click", testGS);
+customerForm.addEventListener("submit", afterSubmit);
